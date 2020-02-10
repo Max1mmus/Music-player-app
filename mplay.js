f3da5389ce82277bf8b1bfe12d5622c5f2391ea0
Max1mmus <56004853+Max1mmus@users.noreply.github.com>
2020-02-10 16:40:13 +0100

    added spaces

--- mplay.js
+++ mplay.js
@@ -1,34 +1,34 @@
 const play = document.querySelector(".play"),
-  track = document.getElementById("audio"),
-  volumeSlider = document.querySelector(".slider"),
-  currentSong = document.getElementById("cSong"),
-  currentArtist = document.getElementById("cArtist"),
-  passedTime = document.getElementById("passedTime"),
-  progressBox = document.getElementById("progress"),
-  frw = document.getElementById("frw"),
-  bck = document.getElementById("bck"),
-  songList = document.querySelector(".playlist"),
-  volIcon = document.getElementById("au");
+    track = document.getElementById("audio"),
+    volumeSlider = document.querySelector(".slider"),
+    currentSong = document.getElementById("cSong"),
+    currentArtist = document.getElementById("cArtist"),
+    passedTime = document.getElementById("passedTime"),
+    progressBox = document.getElementById("progress"),
+    frw = document.getElementById("frw"),
+    bck = document.getElementById("bck"),
+    songList = document.querySelector(".playlist"),
+    volIcon = document.getElementById("au");
 
-const playlist = [{
-    "title": "Funkorama",
-    "artist": "Kevin McLeod",
-    "source": "media/Funkorama.mp3"
-  },
-  {
-    "title": "Cheery Monday",
-    "artist": "Kevin McLeod",
-    "source": "media/cheery.mp3"
-  },
-  {
-    "title": "Aquarium",
-    "artist": "Kevin McLeod",
-    "source": "media/aquarium.mp3"
-  }
+const playlist = [
+    {
+        "title": "Funkorama",
+        "artist": "Kevin McLeod",
+        "source": "media/Funkorama.mp3"
+    },
+    {
+        "title": "Cheery Monday",
+        "artist": "Kevin McLeod",
+        "source": "media/cheery.mp3"
+    },
+    {
+        "title": "Aquarium",
+        "artist": "Kevin McLeod",
+        "source": "media/aquarium.mp3"
+    }
 ];
 let x = 0;
-
-//---Adding event listeners---//
+// ---Adding event listeners---//
 play.addEventListener("click", playPause);
 songList.addEventListener("click", playThis);
 frw.addEventListener("click", playNext);
@@ -36,70 +36,69 @@ bck.addEventListener("click", playPrev);
 volumeSlider.addEventListener("mousemove", setVolume);
 volIcon.addEventListener("click", muteVol);
 
-//---Functions---//
+// ---Functions---//
 function currentS() {
-  track.src = playlist[x].source;
-  currentSong.innerText = playlist[x].title;
-  currentArtist.innerText = playlist[x].artist;
-  playPause();
+    track.src = playlist[x].source;
+    currentSong.innerText = playlist[x].title;
+    currentArtist.innerText = playlist[x].artist;
+    playPause();
 }
 
 function playNext() {
-  x === playlist.length - 1 ? 0 : x++
-  currentS();
+    x === playlist.length - 1 ? 0 : x++;
+    currentS();
 }
 
 function playPrev() {
-  x === 0 ? x = playlist.length - 1 : x--;
-  currentS();
+    x === 0 ? x = playlist.length - 1 : x--;
+    currentS();
 }
 
 function playThis(e) {
-  let targeted = e.target.firstChild.data;
-  for (let i = 0; i < playlist.length; i++) {
-    if (targeted === playlist[i].title) {
-      currentSong.innerText = targeted;
-      track.src = playlist[i].source;
-      currentArtist.innerText = playlist[i].artist;
-      playPause();
+    const targeted = e.target.firstChild.data;
+    for (let i = 0; i < playlist.length; i++) {
+        if (targeted === playlist[i].title) {
+            currentSong.innerText = targeted;
+            track.src = playlist[i].source;
+            currentArtist.innerText = playlist[i].artist;
+            playPause();
+        }
     }
-  }
 }
 
 function playPause() {
 
-  if (track.paused) {
-    play.classList.toggle("change", true);
-    track.play();
-  } else {
-    play.classList.toggle("change", false);
-    track.pause();
-  }
+    if (track.paused) {
+        play.classList.toggle("change", true);
+        track.play();
+    } else {
+        play.classList.toggle("change", false);
+        track.pause();
+    }
 }
 
 track.ontimeupdate = function updateTime() {
-  let min = Math.floor((track.currentTime % 3600) / 60);
-  let sec = Math.floor(track.currentTime % 60);
+    const min = Math.floor((track.currentTime % 3600) / 60),
+        sec = Math.floor(track.currentTime % 60);
+    if (sec < 10) sec = "0" + sec;
 
-  if (sec < 10) sec = "0" + sec;
+    passedTime.innerText = `${min}:${sec}`;
 
-  passedTime.innerText = `${min}:${sec}`;
-
-  if (track.currentTime < track.duration) {
-    progressBox.style.width = Math.round((track.currentTime / track.duration) * 100) + "%";
-  }
-}
+    if (track.currentTime < track.duration) {
+        progressBox.style.width = Math.round((track.currentTime / track.duration) * 100) + "%";
+    }
+};
 
 function setVolume() {
-  track.volume = volumeSlider.value / 100;
+    track.volume = volumeSlider.value / 100;
 }
 
 function muteVol() {
-  if (track.muted) {
-    track.muted = false;
-    volIcon.src = "media/audio.png";
-  } else {
-    track.muted = true;
-    volIcon.src = "media/noau.png";
-  }
+    if (track.muted) {
+        track.muted = false;
+        volIcon.src = "media/audio.png";
+    } else {
+        track.muted = true;
+        volIcon.src = "media/noau.png";
+    }
 }
